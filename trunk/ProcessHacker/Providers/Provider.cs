dd64247219885939a3239f13ea0337a436dacde6
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2008-10-23 09:21:30 +0000

    update docs
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@29 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Providers/Provider.cs
+++ trunk/ProcessHacker/Providers/Provider.cs
@@ -5,23 +5,67 @@ using System.Threading;
 
 namespace ProcessHacker
 {
+    /// <summary>
+    /// A generic delegate which is used when updating the dictionary.
+    /// </summary>
     public delegate void ProviderUpdateOnce();
+
+    /// <summary>
+    /// Represents an invoke method (either Invoke() or BeginInvoke() on a form).
+    /// </summary>
+    /// <param name="method">The method to invoke.</param>
+    /// <param name="args">The arguments to pass to the method.</param>
+    /// <returns></returns>
     public delegate object ProviderInvokeMethod(Delegate method, params object[] args);
+
+    /// <summary>
+    /// Represents a handler called when a dictionary item is added.
+    /// </summary>
+    /// <param name="item">The added item.</param>
     public delegate void ProviderDictionaryAdded(object item);
+
+    /// <summary>
+    /// Represents a handler called when a dictionary item is modified.
+    /// </summary>
+    /// <param name="item">The modified item.</param>
     public delegate void ProviderDictionaryModified(object item);
+
+    /// <summary>
+    /// Represents a handler called when a dictionary item is removed.
+    /// </summary>
+    /// <param name="item">The removed item.</param>
     public delegate void ProviderDictionaryRemoved(object item);
-                                                           
+                                                          
     /// <summary>
     /// Provides services for continuously updating a dictionary.
     /// </summary>
     public class Provider<TKey, TValue>
-    {
+    { 
         private delegate void InvokeDelegate(object item);
-
+                             
+        /// <summary>
+        /// Occurs when the provider needs to update the dictionary (after waiting the duration of the interval).
+        /// </summary>
         protected event ProviderUpdateOnce ProviderUpdate;
+
+        /// <summary>
+        /// Occurs when the provider needs to invoke a method on another thread.
+        /// </summary>
         public ProviderInvokeMethod Invoke;
+
+        /// <summary>
+        /// Occurs when the provider adds an item to the dictionary.
+        /// </summary>
         public event ProviderDictionaryAdded DictionaryAdded;
+
+        /// <summary>
+        /// Occurs when the provider modifies an item in the dictionary.
+        /// </summary>
         public event ProviderDictionaryModified DictionaryModified;
+
+        /// <summary>
+        /// Occurs when the provider removes an item from the dictionary.
+        /// </summary>
         public event ProviderDictionaryRemoved DictionaryRemoved;
                                                            
         Thread _thread;
@@ -32,8 +76,11 @@ namespace ProcessHacker
         bool _useInvoke = false;
         int _interval;
 
+        /// <summary>
+        /// Creates a new instance of the Provider class.
+        /// </summary>
         public Provider()
-        {       
+        {          
             _dictionary = new Dictionary<TKey, TValue>();
 
             _thread = new Thread(new ThreadStart(Update));
@@ -41,29 +88,44 @@ namespace ProcessHacker
             _thread.Start();
         }
 
+        /// <summary>
+        /// Determines whether the provider is currently updating.
+        /// </summary>
         public bool Busy
         {
             get { return _busy; }
         }
-
+          
+        /// <summary>
+        /// Determines whether the provider should update.
+        /// </summary>
         public bool Enabled
         {
             get { return _enabled; }
             set { _enabled = value; }
         }
 
+        /// <summary>
+        /// Determines whether the provider should invoke the DictionaryAdded, Modified and Removed events.
+        /// </summary>
         public bool UseInvoke
         {
             get { return _useInvoke; }
             set { _useInvoke = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the interval to wait between each update.
+        /// </summary>
         public int Interval
         {
             get { return _interval; }
             set { _interval = value; }
         }
 
+        /// <summary>
+        /// Gets the dictionary.
+        /// </summary>
         public Dictionary<TKey, TValue> Dictionary
         {
             get { return _dictionary; }
@@ -88,6 +150,9 @@ namespace ProcessHacker
             }
         }
 
+        /// <summary>
+        /// Updates the dictionary after waiting for the current update operation to finish.
+        /// </summary>
         public void UpdateNow()
         {
             while (_busy) Thread.Sleep(10);
@@ -96,6 +161,9 @@ namespace ProcessHacker
                 ProviderUpdate();
         }
 
+        /// <summary>
+        /// Removes all handlers from the DictionaryAdded, Modified and Removed events.
+        /// </summary>
         public void ClearEvents()
         {
             DictionaryAdded = null;
