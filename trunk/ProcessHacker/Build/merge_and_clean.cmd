20ab953a12d57796ee973799fadd026029473071
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2008-12-20 06:03:20 +0000

    use different strategy for merge_and_clean
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@350 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Build/merge_and_clean.cmd
+++ trunk/ProcessHacker/Build/merge_and_clean.cmd
@@ -1,8 +1,8 @@
 @echo off
 set outd=%~p1
 
-ilmerge /t:winexe /out:"%outd%\ProcessHacker_out.exe" "%outd%\ProcessHacker.exe" "%outd%\Aga.Controls.dll"
-del "%outd%\ProcessHacker.exe"
+rename "%outd%\ProcessHacker.exe" ProcessHacker_in.exe
+ilmerge /t:winexe /out:"%outd%\ProcessHacker.exe" "%outd%\ProcessHacker_in.exe" "%outd%\Aga.Controls.dll"
+del "%outd%\ProcessHacker_in.exe"
 del "%outd%\Aga.Controls.dll"
 del "%outd%\*.pdb"
-rename "%outd%\ProcessHacker_out.exe" ProcessHacker.exe
