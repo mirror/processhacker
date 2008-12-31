965c07dc9e8f08141c9867a9e035dfc582972c94
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2008-12-31 02:15:05 +0000

    smarter merge_and_clean
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@463 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Build/merge_and_clean.cmd
+++ trunk/ProcessHacker/Build/merge_and_clean.cmd
@@ -1,8 +1,14 @@
 @echo off
 set outd=%~p1
 
+@ check if ILMerge is present
+where ilmerge
+if not %errorlevel%==0 goto end
+
 rename "%outd%\ProcessHacker.exe" ProcessHacker_in.exe
 ilmerge /t:winexe /out:"%outd%\ProcessHacker.exe" "%outd%\ProcessHacker_in.exe" "%outd%\Aga.Controls.dll"
 del "%outd%\ProcessHacker_in.exe"
 del "%outd%\Aga.Controls.dll"
+
+:end
 del "%outd%\*.pdb"
