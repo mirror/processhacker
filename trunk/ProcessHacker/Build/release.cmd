bacd5b96e6136bd05d46d5f2fede091118062b48
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-02-26 20:27:56 +0000

    * Added automated creation of the zip of the binaries using 7z command line version * cosmetics to release.cmd
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@739 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Build/release.cmd
+++ trunk/ProcessHacker/Build/release.cmd
@@ -1,18 +1,19 @@
 @ECHO OFF
-SETLOCAL EnableDelayedExpansion
+SETLOCAL
 SET outd=%~p1
 
 ::Copy CHANGELOG.txt, HACKING.txt, LICENSE.txt, README.txt and kprocesshacker.sys
-COPY "%outd%\..\..\..\CHANGELOG.txt" "%outd%\" /V
-COPY "%outd%\..\..\..\HACKING.txt" "%outd%\" /V
-COPY "%outd%\..\..\..\LICENSE.txt" "%outd%\" /V
-COPY "%outd%\..\..\..\README.txt" "%outd%\" /V
-COPY "%outd%\..\..\..\KProcessHacker\i386\kprocesshacker.sys" "%outd%\" /V
+COPY "%outd%\..\..\..\CHANGELOG.txt" "%outd%\" /V >nul 2>&1
+COPY "%outd%\..\..\..\HACKING.txt" "%outd%\" /V >nul 2>&1
+COPY "%outd%\..\..\..\LICENSE.txt" "%outd%\" /V >nul 2>&1
+COPY "%outd%\..\..\..\README.txt" "%outd%\" /V >nul 2>&1
+COPY "%outd%\..\..\..\KProcessHacker\i386\kprocesshacker.sys" "%outd%\" /V >nul 2>&1
 
 ::Clear older files
 DEL "%outd%\ProcessHacker.exe.config" /Q >nul 2>&1
 DEL "%outd%\processhacker-*-setup.exe" /Q >nul 2>&1
 DEL "%outd%\ProcessHacker_in.exe" /Q >nul 2>&1
+DEL "%outd%\processhacker-*-bin.zip" /Q >nul 2>&1
 
 ::Check if ILMerge is present in the default installation location
 IF EXIST "%PROGRAMFILES%\Microsoft\ILMerge\ILMerge.exe" (
@@ -25,7 +26,7 @@ IF EXIST "%PROGRAMFILES%\Microsoft\ILMerge\ILMerge.exe" (
 ::Check if ILMerge is present in PATH
 ilmerge >nul 2>&1
 IF %errorlevel%==9009 (
-	ECHO ILMerge NOT FOUND && GOTO END
+	ECHO ILMerge NOT FOUND && GOTO CLEANUP
 ) ELSE (
 	GOTO ILMergeInPATH
 )
@@ -46,15 +47,22 @@ GOTO Installer
 
 :Installer
 ::Set the path of Inno Setup and compile setup
-FOR /f "tokens=1-5* delims= " %%a IN (
+FOR /f "tokens=1-5*" %%a IN (
 	' REG QUERY "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Inno Setup 5_is1" /v "Inno Setup: App Path" ^| find "App Path" ' 
 ) DO (
 	SET InnoSetupPath=%%f
 )
 
-"%InnoSetupPath%\iscc.exe" /Q /O"%outd%\..\..\bin\Release" "%outd%\..\..\Build\Installer\Process_Hacker_installer.iss"
-GOTO END
+IF NOT EXIST "%InnoSetupPath%" ECHO Inno Setup NOT FOUND&&GOTO CLEANUP
 
-:END
+"%InnoSetupPath%\iscc.exe" /Q /O"%outd%\..\..\bin\Release" "%outd%\..\..\Build\Installer\Process_Hacker_installer.iss"
+ECHO Instaler compiled successfully&&GOTO CLEANUP
+
+:CLEANUP
 ::Clean up the .pdb files
 DEL "%outd%\*.pdb" /Q >nul 2>&1
+GOTO ZIP
+
+:ZIP
+"%outd%\..\..\Build\7za\7za.exe" a -tzip "processhacker-svn-bin.zip" "*" -x!*setup.exe -mx=9 >nul
+ECHO ZIP created successfully
