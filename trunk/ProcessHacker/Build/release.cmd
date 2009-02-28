dbe44e609b51039902324a46feb6f1b203a6344f
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-02-28 13:12:06 +0000

    release.cmd: Many changes and improvements (Thanks to Yzφwl)
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@777 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Build/release.cmd
+++ trunk/ProcessHacker/Build/release.cmd
@@ -1,79 +1,56 @@
 @ECHO OFF
+::Original script by wj32.
+::Modifications and additions by XhmikosR and Yzöwl.
 SETLOCAL
 SET outd=%~p1
+PUSHD %outd%
 
-::Copy CHANGELOG.txt, LICENSE.txt, README.txt and kprocesshacker.sys
-COPY "%outd%\..\..\..\CHANGELOG.txt" "%outd%\" /V >nul 2>&1
-COPY "%outd%\..\..\..\LICENSE.txt" "%outd%\" /V >nul 2>&1
-COPY "%outd%\..\..\..\README.txt" "%outd%\" /V >nul 2>&1
-COPY "%outd%\..\..\..\KProcessHacker\i386\kprocesshacker.sys" "%outd%\" /V >nul 2>&1
+::Copy CHANGELOG.txt, LICENSE.txt, README.txt and kprocesshacker.sys to the
+::"Release" folder
+FOR %%a IN (
+	CHANGELOG.txt LICENSE.txt README.txt KProcessHacker\i386\kprocesshacker.sys
+	) DO COPY "..\..\..\%%a" >NUL
 
-::Clear older files
-DEL "%outd%\ProcessHacker.exe.config" /Q >nul 2>&1
-DEL "%outd%\processhacker-*-setup.exe" /Q >nul 2>&1
-DEL "%outd%\ProcessHacker_in.exe" /Q >nul 2>&1
-DEL "%outd%\processhacker-bin.zip" /Q >nul 2>&1
+::Clear older files present in "Release" folder
+DEL/f/a ProcessHacker.exe.config "processhacker-*-setup.exe"^
+ ProcessHacker_in.exe processhacker-bin.zip >NUL 2>&1
 
-::Check if ILMerge is present in the default installation location
-IF EXIST "%PROGRAMFILES%\Microsoft\ILMerge\ILMerge.exe" (
-	SET ILMergePath=%PROGRAMFILES%\Microsoft\ILMerge\ILMerge.exe&&GOTO :ILMerge
-) ELSE (
-	GOTO :SearchILMergeInPATH
-)
+::Check if ILMerge is present in the default installation location or in PATH
+SET ILMergePath="%PROGRAMFILES%\Microsoft\ILMerge\ILMerge.exe"
+IF NOT EXIST %ILMergePath% (FOR %%a IN (ILMerge.exe) DO IF %%~$PATH:a' NEQ ' (
+		SET ILMergePath="%%~$PATH:a") ELSE (SET "N_=T"
+			ECHO:ILMerge IS NOT INSTALLED!!!&&(GOTO CLEANUP)))
 
-:SearchILMergeInPATH
-::Check if ILMerge is present in PATH
-SET "IL_=ILMerge.exe"
-SET "MSGIL_=ILMerge IS NOT INSTALLED"
+::Merge "Aga.Controls.dll" with "ProcessHacker.exe" using ILMerge
+REN ProcessHacker.exe ProcessHacker_in.exe
+%ILMergePath% /t:winexe /out:ProcessHacker.exe ProcessHacker_in.exe^
+ Aga.Controls.dll&&DEL/f/a ProcessHacker_in.exe Aga.Controls.dll >NUL 2>&1
 
-FOR %%# IN (%IL_%) DO IF %%~$PATH:#' EQU ' (
-	ECHO:%MSGIL_%&&GOTO :END
-) ELSE (
-	GOTO :ILMergeInPATH
-)
-
-:ILMergeInPath
-REN "%outd%\ProcessHacker.exe" ProcessHacker_in.exe
-ilmerge /t:winexe /out:"%outd%\ProcessHacker.exe" "%outd%\ProcessHacker_in.exe" "%outd%\Aga.Controls.dll"
-DEL "%outd%\ProcessHacker_in.exe" /Q >nul 2>&1
-DEL "%outd%\Aga.Controls.dll" /Q >nul 2>&1
-GOTO :Installer
-
-:ILMerge
-rename "%outd%\ProcessHacker.exe" ProcessHacker_in.exe
-"%ILMergePath%" /t:winexe /out:"%outd%\ProcessHacker.exe" "%outd%\ProcessHacker_in.exe" "%outd%\Aga.Controls.dll"
-DEL "%outd%\ProcessHacker_in.exe" /Q >nul 2>&1
-DEL "%outd%\Aga.Controls.dll" /Q >nul 2>&1
-GOTO :Installer
-
-:Installer
-::Set the path of Inno Setup and compile setup
+::Set the path of Inno Setup and compile installer
 SET "U_=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 SET "I_=Inno Setup"
-
 SET "A_=%I_% 5"
-SET "M_=Inno Setup IS NOT INSTALLED"
-
+SET "M_=Inno Setup IS NOT INSTALLED!!!"
 FOR /f "delims=" %%a IN (
-'REG QUERY "%U_%\%A_%_is1" /v "%I_%: App Path"2^>Nul^|FIND "REG_"') DO (
-SET "InnoSetupPath=%%a
-SET "InnoSetupPath=%%a"&Call :Sub %%InnoSetupPath:*Z=%%)
+	'REG QUERY "%U_%\%A_%_is1" /v "%I_%: App Path"2^>Nul^|FIND "REG_"') DO (
+	SET "InnoSetupPath=%%a"&Call :Sub %%InnoSetupPath:*Z=%%)
 
-IF NOT DEFINED InnoSetupPath ECHO:%M_%&&GOTO :END
-
-"%InnoSetupPath%\iscc.exe" /Q /O"%outd%\..\..\bin\Release" "%outd%\..\..\Build\Installer\Process_Hacker_installer.iss"
-ECHO Instaler compiled successfully&&GOTO :CLEANUP
+IF DEFINED InnoSetupPath ("%InnoSetupPath%\iscc.exe" /Q /O"..\..\bin\Release"^
+ "..\..\Build\Installer\Process_Hacker_installer.iss"&&(
+	ECHO:Instaler compiled successfully!)) ELSE (ECHO:%M_%)
 
 :CLEANUP
-::Clean up the .pdb files
-DEL "%outd%\*.pdb" /Q >nul 2>&1
-GOTO :ZIP
+::Clean up the .pdb files in "Release" folder
+DEL/f/a/q *.pdb >NUL 2>&1
 
-:ZIP
-"%outd%\..\..\Build\7za\7za.exe" a -tzip "processhacker-bin.zip" "*" -x!*setup.exe -mx=9 >nul
-ECHO ZIP created successfully
+::ZIP the binaries
+IF NOT DEFINED N_ (START "" /B /WAIT ..\..\Build\7za\7za.exe a -tzip^
+ "processhacker-bin.zip" "*" -x!*setup.exe -mx=9 >NUL&&(
+	ECHO:ZIP created successfully!))
 
 :END
+>NUL PING -n 6 127.0.0.1
+GOTO :EOF
 
 :Sub
 SET InnoSetupPath=%*
