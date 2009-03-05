810597736fe3cd4d22ab7098b9d06c7095b412e2
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-03-05 06:35:18 +0000

    updated dates
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@829 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Providers/HandleProvider.cs
+++ trunk/ProcessHacker/Providers/HandleProvider.cs
@@ -2,7 +2,7 @@
  * Process Hacker - 
  *   handle provider
  * 
- * Copyright (C) 2008 wj32
+ * Copyright (C) 2008-2009 wj32
  * 
  * This file is part of Process Hacker.
  * 
--- trunk/ProcessHacker/Providers/ThreadProvider.cs
+++ trunk/ProcessHacker/Providers/ThreadProvider.cs
@@ -2,7 +2,7 @@
  * Process Hacker - 
  *   thread provider
  * 
- * Copyright (C) 2008 wj32
+ * Copyright (C) 2008-2009 wj32
  * 
  * This file is part of Process Hacker.
  * 
