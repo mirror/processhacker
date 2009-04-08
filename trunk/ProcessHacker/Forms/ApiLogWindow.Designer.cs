6cf9fac41919ad7537a5c26a76f81ce755c67b6e
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-04-08 07:18:18 +0000

    API log updates smoothly
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@1034 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Forms/ApiLogWindow.Designer.cs
+++ trunk/ProcessHacker/Forms/ApiLogWindow.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listLog = new System.Windows.Forms.ListView();
             this.columnTime = new System.Windows.Forms.ColumnHeader();
+            this.columnProcess = new System.Windows.Forms.ColumnHeader();
             this.columnFunction = new System.Windows.Forms.ColumnHeader();
             this.columnInformation = new System.Windows.Forms.ColumnHeader();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // listLog
@@ -41,34 +44,51 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.listLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnTime,
+            this.columnProcess,
             this.columnFunction,
             this.columnInformation});
+            this.listLog.FullRowSelect = true;
+            this.listLog.HideSelection = false;
             this.listLog.Location = new System.Drawing.Point(12, 12);
             this.listLog.Name = "listLog";
-            this.listLog.Size = new System.Drawing.Size(587, 371);
+            this.listLog.Size = new System.Drawing.Size(870, 578);
             this.listLog.TabIndex = 0;
             this.listLog.UseCompatibleStateImageBehavior = false;
             this.listLog.View = System.Windows.Forms.View.Details;
+            this.listLog.VirtualMode = true;
+            this.listLog.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.listLog_RetrieveVirtualItem);
             // 
             // columnTime
             // 
             this.columnTime.Text = "Time";
+            this.columnTime.Width = 120;
+            // 
+            // columnProcess
+            // 
+            this.columnProcess.Text = "Process";
+            this.columnProcess.Width = 100;
             // 
             // columnFunction
             // 
             this.columnFunction.Text = "Function";
-            this.columnFunction.Width = 100;
+            this.columnFunction.Width = 140;
             // 
             // columnInformation
             // 
             this.columnInformation.Text = "Information";
-            this.columnInformation.Width = 400;
+            this.columnInformation.Width = 500;
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Interval = 1000;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
             // 
             // ApiLogWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 494);
+            this.ClientSize = new System.Drawing.Size(894, 602);
             this.Controls.Add(this.listLog);
             this.Name = "ApiLogWindow";
             this.Text = "API Log";
@@ -83,5 +103,7 @@
         private System.Windows.Forms.ColumnHeader columnTime;
         private System.Windows.Forms.ColumnHeader columnFunction;
         private System.Windows.Forms.ColumnHeader columnInformation;
+        private System.Windows.Forms.ColumnHeader columnProcess;
+        private System.Windows.Forms.Timer timerUpdate;
     }
 }
--- trunk/ProcessHacker/Forms/ApiLogWindow.cs
+++ trunk/ProcessHacker/Forms/ApiLogWindow.cs
@@ -27,15 +27,26 @@ namespace ProcessHacker.Forms
             public short ArgumentLength;
         }
 
+        private List<string[]> _items = new List<string[]>();
         private Thread _pipeThread;
-        private Win32.NamedPipeHandle _namedPipe = new Win32.NamedPipeHandle("\\\\.\\Pipe\\AlLogPipe",
-            Win32.PIPE_ACCESS_MODE.FILE_FLAG_FIRST_PIPE_INSTANCE | Win32.PIPE_ACCESS_MODE.PIPE_ACCESS_INBOUND,
+        private static Win32.NamedPipeHandle _namedPipe = new Win32.NamedPipeHandle("\\\\.\\Pipe\\AlLogPipe",
+            Win32.PIPE_ACCESS_MODE.PIPE_ACCESS_INBOUND,
             0,
             255, 0, 512, 1000);
 
         public ApiLogWindow()
         {
             InitializeComponent();
+
+            listLog.SetDoubleBuffered(true);
+            listLog.SetTheme("explorer");
+
+            try
+            {
+                _namedPipe.Disconnect();
+            }
+            catch
+            { }
 
             _pipeThread = new Thread(new ThreadStart(this.ProcessPipe));
             _pipeThread.Start();
@@ -50,7 +61,15 @@ namespace ProcessHacker.Forms
         {
             while (true)
             {
-                _namedPipe.Connect();
+                try
+                {
+                    _namedPipe.Connect();
+                }
+                catch
+                {
+                    Thread.Sleep(1000);
+                    continue;
+                }
 
                 int length = Misc.BytesToInt(_namedPipe.Read(4), Misc.Endianness.Little);
                 byte[] data = new byte[length - 4];
@@ -83,18 +102,28 @@ namespace ProcessHacker.Forms
 
                 br.Close();
 
-                this.BeginInvoke(new MethodInvoker(() =>
-                    {
-                        listLog.Items.Add(new ListViewItem(new string[]
-                        {
-                            DateTime.Now.ToString(),
-                            function,
-                            args.ToString()
-                        }));
-                    }));
+                _items.Add(new string[]
+                {
+                    DateTime.Now.ToString(),
+                    Program.HackerWindow.ProcessProvider.Dictionary.ContainsKey(pid) ? 
+                    Program.HackerWindow.ProcessProvider.Dictionary[pid].Name + " (" + pid.ToString() + ")" : 
+                    "(" + pid.ToString() + ")",
+                    function,
+                    args.ToString()
+                });
 
                 _namedPipe.Disconnect();
             }
+        }
+
+        private void listLog_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = new ListViewItem(_items[e.ItemIndex]);
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            listLog.VirtualListSize = _items.Count;
         }
     }
 }
