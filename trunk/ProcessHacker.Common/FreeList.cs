249345130d58df27a451ad3f44450e7e43c4b9e5
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-06-07 06:50:14 +0000

    added basic FreeList
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@1383 21ef857c-d57f-4fe0-8362-d861dc6d29cd

---
+++ trunk/ProcessHacker.Common/FreeList.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace ProcessHacker.Common
+{
+    /// <summary>
+    /// Manages a list of free objects that can be re-used.
+    /// </summary>
+    public class FreeList<T>
+        where T : IResettable, new()
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+        private int _maximumCount = 0;
+
+        public int MaximumCount
+        {
+            get { return _maximumCount; }
+            set { _maximumCount = value; }
+        }
+
+        public T Allocate()
+        {
+            lock (_list)
+            {
+                if (_list.Count == 0)
+                    return this.AllocateNew();
+                T obj = _list.First.Value;
+                _list.RemoveFirst();
+                return obj;
+            }
+        }
+
+        private T AllocateNew()
+        {
+            T obj = new T();
+            obj.ResetObject();
+            return obj;
+        }
+
+        public void Clear()
+        {
+            lock (_list)
+                _list.Clear();
+        }
+
+        public void Free(T obj)
+        {
+            lock (_list)
+            {
+                if (_list.Count < _maximumCount || _maximumCount == 0)
+                    _list.AddFirst(obj);
+            }
+        }
+    }
+}
---
+++ trunk/ProcessHacker.Common/IResettable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessHacker.Common
+{
+    public interface IResettable
+    {
+        void ResetObject();
+    }
+}
