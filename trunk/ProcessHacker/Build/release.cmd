a2e020919775545e56bf8b69f50139487f07ff90
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-07-14 02:29:03 +0000

    build script now creates a PDB zip
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@1598 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Build/release.cmd
+++ trunk/ProcessHacker/Build/release.cmd
@@ -8,13 +8,14 @@ PUSHD %outd%
 :: Copy CHANGELOG.txt, LICENSE.txt, README.txt and kprocesshacker.sys to the
 :: "Release" folder
 FOR %%a IN (
-	"CHANGELOG.txt" "LICENSE.txt" "README.txt" "KProcessHacker\i386\kprocesshacker.sys" 
+	"CHANGELOG.txt" "LICENSE.txt" "README.txt"
+    "KProcessHacker\i386\kprocesshacker.sys" 
     "NProcessHacker\Release\NProcessHacker.dll"
 	) DO COPY "..\..\..\%%a" >NUL
 
 :: Clear older files present in "Release" folder
 DEL/f/a "ProcessHacker.exe.config" "processhacker-*-setup.exe"^
- "ProcessHacker_in.exe" "processhacker-bin.zip" >NUL 2>&1
+ "Assistant.dll" "processhacker-*.zip" >NUL 2>&1
 
 :: Check if ILMerge is present in the default installation location or in PATH
 SET ILMergePath="%PROGRAMFILES%\Microsoft\ILMerge\ILMerge.exe"
@@ -22,24 +23,27 @@ IF NOT EXIST %ILMergePath% (FOR %%a IN (ILMerge.exe) DO IF %%~$PATH:a' NEQ ' (
 		SET ILMergePath="%%~$PATH:a") ELSE (SET "N_=T"
 			ECHO:ILMerge IS NOT INSTALLED!!!&&(GOTO CLEANUP)))
 
-:: Rename "ProcessHacker.exe" and "Assistant.exe" first
-REN "ProcessHacker.exe" "ProcessHacker_in.exe"^
- && REN "Assistant.exe" "Assistant_in.exe"
-ECHO.
-
 SET RequiredDLLs="Aga.Controls.dll" "ProcessHacker.Common.dll"^
  "ProcessHacker.Native.dll"
+
+:: Create a temporary directory for the merged files
+mkdir tmp
+
 :: Merge DLLs with "Assistant.exe"
 
-%ILMergePath% /t:exe /out:"Assistant.exe" "Assistant_in.exe"^
+%ILMergePath% /t:exe /out:"tmp\Assistant.exe" "Assistant.exe"^
  %RequiredDLLs% && ECHO:DLLs merged successfully with Assistant.exe!
 
-:: Merge "Aga.Controls.dll" with "ProcessHacker.exe" using ILMerge
-%ILMergePath% /t:winexe /out:"ProcessHacker.exe" "ProcessHacker_in.exe"^
+:: Merge DLLs with "ProcessHacker.exe" using ILMerge
+%ILMergePath% /t:winexe /out:"tmp\ProcessHacker.exe" "ProcessHacker.exe"^
  %RequiredDLLs% && ECHO:DLLs merged successfully with ProcessHacker.exe!
 
-DEL/f/a "ProcessHacker_in.exe" "Assistant_in.exe" %RequiredDLLs%^
- "ProcessHacker.Common.xml" "ProcessHacker.Native.xml" >NUL 2>&1
+:: Delete the existing EXEs and PDBs
+del ProcessHacker.exe Assistant.exe *.pdb
+:: Copy the merged files (two EXEs and two PDBs) back into this directory
+move tmp\* .\
+
+DEL/f/a %RequiredDLLs% "ProcessHacker.Common.xml" "ProcessHacker.Native.xml" >NUL 2>&1
 
 :: Set the path of Inno Setup and compile installer
 SET "U_=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
@@ -54,14 +58,25 @@ IF DEFINED InnoSetupPath ("%InnoSetupPath%\iscc.exe" /Q /O"..\..\bin\Release"^
  "..\..\Build\Installer\Process_Hacker_installer.iss"&&(
 	ECHO:Installer compiled successfully!)) ELSE (ECHO:%M_%)
 
-:CLEANUP
-:: Clean up the .pdb files in "Release" folder
-DEL /f/a/q *.pdb >NUL 2>&1
+:: Delete the temporary directory
+rmdir tmp
 
-:: ZIP the binaries
-IF NOT DEFINED N_ (START "" /B /WAIT "..\..\Build\7za\7za.exe" a -tzip^
- "processhacker-bin.zip" "*" -x!*setup.exe -mx=9 >NUL&&(
+:: ZIP the files
+IF NOT DEFINED N_ (START "" /B /WAIT "..\..\Build\7za\7za.exe" a -tzip -mx=9^
+ "processhacker-bin.zip"^
+ "Assistant.exe" "base.txt" "CHANGELOG.txt" "Help.htm" "kprocesshacker.sys"^
+ "LICENSE.txt" "NProcessHacker.dll" "ProcessHacker.exe" "README.txt" "structs.txt"^
+ >NUL&&(
 	ECHO:ZIP created successfully!))
+
+:CLEANUP
+
+:: Make a PDB zip
+"..\..\Build\7za\7za.exe" a -tzip -mx=9 "processhacker-pdb.zip"^
+ "*.pdb"^
+ "..\..\..\KProcessHacker\i386\kprocesshacker.pdb"^
+ "..\..\..\NProcessHacker\Release\NProcessHacker.pdb"^
+ >NUL&&(ECHO:PDB ZIP created successfully!)
 
 :END
 GOTO :EOF
