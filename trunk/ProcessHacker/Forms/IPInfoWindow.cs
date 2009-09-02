e3d1bf9591082fef1a35d8e8ae5b3ff6c89a6e48
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-09-02 06:31:31 +0000

    * updated CHANGELOG.txt * added copyright header to IPInfoWindow.cs; unused using cleanup
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@1819 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Forms/IPInfoWindow.cs
+++ trunk/ProcessHacker/Forms/IPInfoWindow.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using System.Windows.Forms;
-using ProcessHacker.Common;
+﻿/*
+ * Process Hacker - 
+ *   IP information window
+ * 
+ * Copyright (C) 2009 dmex
+ * 
+ * This file is part of Process Hacker.
+ * 
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
-using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.IO;
+using System.Windows.Forms;
 
 namespace ProcessHacker
 {
