f1744333dda6648f9247b5b227b515cc5de32d01
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-09-27 05:18:39 +0000

    * added bytes transferred display * switched from average speed to localized speed
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@1963 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/ProcessHacker/Forms/VirusTotalUploaderWindow.Designer.cs
+++ trunk/ProcessHacker/Forms/VirusTotalUploaderWindow.Designer.cs
@@ -1,4 +1,5 @@
-﻿
+﻿namespace ProcessHacker
+{
     partial class VirusTotalUploaderWindow
     {
         /// <summary>
@@ -28,36 +29,37 @@
         private void InitializeComponent()
         {
             this.LogoImg = new System.Windows.Forms.PictureBox();
-            this.fileLabel = new System.Windows.Forms.Label();
+            this.labelFile = new System.Windows.Forms.Label();
             this.speedLabel = new System.Windows.Forms.Label();
             this.UploadWorker = new System.ComponentModel.BackgroundWorker();
-            this.progressBar2 = new ProcessHacker.Components.ProgressBar();
+            this.progressBar = new ProcessHacker.Components.ProgressBar();
+            this.buttonCancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.LogoImg)).BeginInit();
             this.SuspendLayout();
             // 
             // LogoImg
             // 
             this.LogoImg.Image = global::ProcessHacker.Properties.Resources.VirusTotal_logo;
-            this.LogoImg.Location = new System.Drawing.Point(319, 8);
+            this.LogoImg.Location = new System.Drawing.Point(315, 12);
             this.LogoImg.Name = "LogoImg";
             this.LogoImg.Size = new System.Drawing.Size(103, 36);
             this.LogoImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LogoImg.TabIndex = 0;
             this.LogoImg.TabStop = false;
             // 
-            // fileLabel
+            // labelFile
             // 
-            this.fileLabel.AutoSize = true;
-            this.fileLabel.Location = new System.Drawing.Point(9, 9);
-            this.fileLabel.Name = "fileLabel";
-            this.fileLabel.Size = new System.Drawing.Size(23, 13);
-            this.fileLabel.TabIndex = 2;
-            this.fileLabel.Text = "File";
+            this.labelFile.AutoSize = true;
+            this.labelFile.Location = new System.Drawing.Point(12, 9);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(23, 13);
+            this.labelFile.TabIndex = 2;
+            this.labelFile.Text = "File";
             // 
             // speedLabel
             // 
             this.speedLabel.AutoSize = true;
-            this.speedLabel.Location = new System.Drawing.Point(9, 40);
+            this.speedLabel.Location = new System.Drawing.Point(12, 35);
             this.speedLabel.Name = "speedLabel";
             this.speedLabel.Size = new System.Drawing.Size(38, 13);
             this.speedLabel.TabIndex = 3;
@@ -71,22 +73,37 @@
             this.UploadWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.UploadWorker_RunWorkerCompleted);
             this.UploadWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.UploadWorker_ProgressChanged);
             // 
-            // progressBar2
+            // progressBar
             // 
-            this.progressBar2.Location = new System.Drawing.Point(12, 59);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.ProgressBarColor = System.Drawing.Color.Blue;
-            this.progressBar2.Size = new System.Drawing.Size(410, 17);
-            this.progressBar2.TabIndex = 4;
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 62);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressBarColor = System.Drawing.Color.Blue;
+            this.progressBar.Size = new System.Drawing.Size(325, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonCancel.Location = new System.Drawing.Point(343, 62);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // VirusTotalUploaderWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 88);
-            this.Controls.Add(this.progressBar2);
+            this.ClientSize = new System.Drawing.Size(430, 97);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.speedLabel);
-            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.labelFile);
             this.Controls.Add(this.LogoImg);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -95,6 +112,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "VirusTotal Uploader";
             this.Load += new System.EventHandler(this.VirusTotalUploaderWindow_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VirusTotalUploaderWindow_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.LogoImg)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -104,8 +122,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox LogoImg;
-        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Label labelFile;
         private System.Windows.Forms.Label speedLabel;
         private System.ComponentModel.BackgroundWorker UploadWorker;
-        private ProcessHacker.Components.ProgressBar progressBar2;
+        private ProcessHacker.Components.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonCancel;
     }
+}
--- trunk/ProcessHacker/Forms/VirusTotalUploaderWindow.cs
+++ trunk/ProcessHacker/Forms/VirusTotalUploaderWindow.cs
@@ -22,11 +22,11 @@
 
 using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
-using ProcessHacker;
 using ProcessHacker.Common;
 
 /* ProcessHacker VirusTotal Implementation Authorized by:
@@ -35,134 +35,150 @@ using ProcessHacker.Common;
  * 26/09/2009 - 2:39PM
  */
 
-public partial class VirusTotalUploaderWindow : Form
-{  
-    string url;  
-    string filepath;
-    string processName;
-     
-    DateTime StartTime;
-    long bytesPerSecond;
- 
-    public VirusTotalUploaderWindow(string procName, string procPath)  
+namespace ProcessHacker
+{
+    public partial class VirusTotalUploaderWindow : Form
     {
-        InitializeComponent();
- 
-        processName = procName;
-        filepath = procPath;
+        string url;
+        string filepath;
+        string processName;
 
-        this.Icon = Program.HackerWindow.Icon;
-    }
-      
-    private void VirusTotalUploaderWindow_Load(object sender, EventArgs e)
-    {
-        fileLabel.Text = string.Format("Uploading {0} to VirusTotal", processName);
-  
-        WebClient vtId = new WebClient();
-        vtId.Headers.Add("User-Agent", "Process Hacker " + Application.ProductVersion);
- 
-        url = "http://www.virustotal.com/vt/en/recepcionf?" + vtId.DownloadString("http://www.virustotal.com/vt/en/identificador");
- 
-        StartTime = DateTime.Now;
- 
-        UploadWorker.RunWorkerAsync();  
-    }
- 
-    private void UploadWorker_DoWork(object sender, DoWorkEventArgs e)
-    {  
-        Uri uri = new Uri(url);
+        long bytesPerSecond;
+        long bytesTransferred;
 
-        string boundary = "----------" + DateTime.Now.Ticks.ToString("x");
-
-        HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(uri);
-
-        webrequest.UserAgent = "ProcessHacker " + Application.ProductVersion;
-        webrequest.ContentType = "multipart/form-data; boundary=" + boundary;
-        webrequest.Timeout = System.Threading.Timeout.Infinite;
-        webrequest.KeepAlive = true; 
-        webrequest.Method = WebRequestMethods.Http.Post;
-
-        // Build up the 'post' message header
-        StringBuilder sb = new StringBuilder();  
-        sb.Append("--");
-        sb.Append(boundary);
-        sb.Append("\r\n");
-        sb.Append(@"Content-Disposition: form-data; name=""archivo""; filename=" + processName + "");
-        sb.Append("\r\n");  
-        sb.Append("Content-Type: application/octet-stream");
-        sb.Append("\r\n");
-        sb.Append("\r\n");
- 
-        string postHeader = sb.ToString();
-        byte[] postHeaderBytes = Encoding.UTF8.GetBytes(postHeader);
-           
-        // Build the trailing boundary string as a byte array
-        // ensuring the boundary appears on a line by itself
-        byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
-
-        if (UploadWorker.CancellationPending)
-            return;
-
-        using (FileStream fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+        public VirusTotalUploaderWindow(string procName, string procPath)
         {
-            webrequest.ContentLength = postHeaderBytes.Length + fileStream.Length + boundaryBytes.Length;
+            InitializeComponent();
 
-            using (Stream requestStream = webrequest.GetRequestStream())
-            {
-                // Write out our post header
-                requestStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
-                // Write out the file contents
-                byte[] buffer = new Byte[checked((uint)Math.Min(256, (int)fileStream.Length))];
+            processName = procName;
+            filepath = procPath;
 
-                int bytesRead = 0;
-                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
-                {
-                    if (UploadWorker.CancellationPending)
-                        break;
-
-                    requestStream.Write(buffer, 0, bytesRead);
-
-                    int progress = (int)(((float)fileStream.Position / (float)fileStream.Length) * 100);
-
-                    TimeSpan totalTime = DateTime.Now - StartTime;
-
-                    if (totalTime.TotalMilliseconds > 0)
-                        bytesPerSecond = fileStream.Position * 1000 / (long)totalTime.TotalMilliseconds;
-
-                    //todo: add transfered data/filesize
-
-                    UploadWorker.ReportProgress(progress);
-                }
-
-                // Write out the trailing boundary
-                requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
-            }
+            this.Icon = Program.HackerWindow.Icon;
         }
 
-        WebResponse response = webrequest.GetResponse();
+        private void VirusTotalUploaderWindow_Load(object sender, EventArgs e)
+        {
+            labelFile.Text = string.Format("Uploading {0} to VirusTotal", processName);
 
-        //Stream s = responce.GetResponseStream(); 
-        //StreamReader sr = new StreamReader(s);
-        //sr.ReadToEnd();
+            WebClient vtId = new WebClient();
+            vtId.Headers.Add("User-Agent", "Process Hacker " + Application.ProductVersion);
 
-        //Return the response URL 
-        e.Result = response.ResponseUri.AbsoluteUri; 
-    }
+            url = "http://www.virustotal.com/vt/en/recepcionf?" + vtId.DownloadString("http://www.virustotal.com/vt/en/identificador");
 
-    private void UploadWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)  
-    {
-        speedLabel.Text = Utils.FormatSize(bytesPerSecond) + "/s"; 
-        progressBar2.Value = e.ProgressPercentage;  
-    }
+            UploadWorker.RunWorkerAsync();
+        }
 
-    private void UploadWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)  
-    {
-        //TODO: future additions will parse the page and  
-        //display the appropriate infomation but for now just mirror  
-        //the functionality of the VirusTotal desktop client and
-        //launch the URL in the default browser
-        System.Diagnostics.Process.Start(e.Result.ToString());
+        private void VirusTotalUploaderWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (UploadWorker.IsBusy)
+                UploadWorker.CancelAsync();
+        }
 
-        this.Close();
+        private void UploadWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Uri uri = new Uri(url);
+
+            string boundary = "----------" + DateTime.Now.Ticks.ToString("x");
+
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(uri);
+
+            webrequest.UserAgent = "ProcessHacker " + Application.ProductVersion;
+            webrequest.ContentType = "multipart/form-data; boundary=" + boundary;
+            webrequest.Timeout = System.Threading.Timeout.Infinite;
+            webrequest.KeepAlive = true;
+            webrequest.Method = WebRequestMethods.Http.Post;
+
+            // Build up the 'post' message header
+            StringBuilder sb = new StringBuilder();
+            sb.Append("--");
+            sb.Append(boundary);
+            sb.Append("\r\n");
+            sb.Append(@"Content-Disposition: form-data; name=""archivo""; filename=" + processName + "");
+            sb.Append("\r\n");
+            sb.Append("Content-Type: application/octet-stream");
+            sb.Append("\r\n");
+            sb.Append("\r\n");
+
+            string postHeader = sb.ToString();
+            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(postHeader);
+
+            // Build the trailing boundary string as a byte array
+            // ensuring the boundary appears on a line by itself
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            if (UploadWorker.CancellationPending)
+                return;
+
+            using (FileStream fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                webrequest.ContentLength = postHeaderBytes.Length + fileStream.Length + boundaryBytes.Length;
+
+                using (Stream requestStream = webrequest.GetRequestStream())
+                {
+                    // Write out our post header
+                    requestStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
+                    // Write out the file contents
+                    byte[] buffer = new Byte[checked((uint)Math.Min(256, (int)fileStream.Length))];
+
+                    int bytesRead = 0;
+                    Stopwatch stopwatch = new Stopwatch();
+
+                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        if (UploadWorker.CancellationPending)
+                            break;
+
+                        stopwatch.Start();
+                        requestStream.Write(buffer, 0, bytesRead);
+                        stopwatch.Stop();
+
+                        int progress = (int)(((float)fileStream.Position / (float)fileStream.Length) * 100);
+
+                        if (stopwatch.ElapsedMilliseconds > 0)
+                            bytesPerSecond = fileStream.Position * 1000 / stopwatch.ElapsedMilliseconds;
+
+                        bytesTransferred = fileStream.Position;
+
+                        stopwatch.Reset();
+
+                        UploadWorker.ReportProgress(progress);
+                    }
+
+                    // Write out the trailing boundary
+                    requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+                }
+            }
+
+            WebResponse response = webrequest.GetResponse();
+
+            //Stream s = responce.GetResponseStream(); 
+            //StreamReader sr = new StreamReader(s);
+            //sr.ReadToEnd();
+
+            //Return the response URL 
+            e.Result = response.ResponseUri.AbsoluteUri;
+        }
+
+        private void UploadWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            speedLabel.Text = "Uploaded " + Utils.FormatSize(bytesTransferred) + " (" + Utils.FormatSize(bytesPerSecond) + "/s)";
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void UploadWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //TODO: future additions will parse the page and  
+            //display the appropriate infomation but for now just mirror  
+            //the functionality of the VirusTotal desktop client and
+            //launch the URL in the default browser
+            Program.TryStart(e.Result.ToString());
+
+            this.Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
