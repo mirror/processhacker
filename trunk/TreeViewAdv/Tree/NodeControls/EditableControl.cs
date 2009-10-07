7f2d18ae18d438c7305dd133c672f10182435465
dmex04 <dmex04@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-10-07 23:38:36 +0000

    set timer from 500 to 1000, prevented system from entering sleep mode
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@2097 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- trunk/TreeViewAdv/Tree/NodeControls/EditableControl.cs
+++ trunk/TreeViewAdv/Tree/NodeControls/EditableControl.cs
@@ -39,7 +39,7 @@ namespace Aga.Controls.Tree.NodeControls
 		protected EditableControl()
 		{
 			_timer = new Timer();
-			_timer.Interval = 500;
+			_timer.Interval = 1000;
 			_timer.Tick += new EventHandler(TimerTick);
 		}
 
