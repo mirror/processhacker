af09468b65c28885446f27defd6924e79c6c6890
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-06-22 03:14:34 +0000

    Updated PH 1.x solution to vs2010, updated CLR runtime to 4.0
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@4250 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- 1.x/trunk/ProcessHacker.Common/ExtensionAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public class ExtensionAttribute : Attribute
-    { }
-}
