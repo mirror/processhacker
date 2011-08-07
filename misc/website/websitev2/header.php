023aadc31c25ae0a9f294b95e092ad0d14b5cf9f
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-08-07 09:57:41 +0000

    switched doctype to html5, removed redundant css
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@4408 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/header.php
+++ misc/website/websitev2/header.php
@@ -1,15 +1,15 @@
 <?php ob_start("ob_gzhandler"); header('Content-Type: text/html; charset=UTF-8'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><!-- <!DOCTYPE html> -->
+<!DOCTYPE html> <!-- <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> -->
 <html lang="en">
 	<head>
 		<meta name="description" content="A free and open source process viewer with powerful process termination and memory searching/editing capabilities." >
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
-		<!-- <meta charset="utf-8" > -->
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
 		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 		<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
 		
-		<link rel="icon" href="/images/favicon.ico" >
-		<link rel="stylesheet" type="text/css" media="screen, print" href="/css/stylesheet.css" >
+		<link rel="icon" href="/images/favicon.ico">
+		<link rel="stylesheet" type="text/css" media="screen, print" href="/css/stylesheet.css">
 		<link rel="stylesheet" type="text/css" media="screen" href="/css/lytebox.css">
 		
 		<script type="text/javascript" language="javascript" src="/scripts/lytebox.js"></script>
