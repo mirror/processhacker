a7f28a09ecb48eed1bff4f6ce287cca66bfa4d7a
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-09-02 11:55:49 +0000

    updated for 2.21 release
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@4615 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/config.php
+++ misc/website/websitev2/config.php
@@ -1,20 +1,20 @@
 <?php
-	define('LATEST_PH_VERSION', '2.20');
-	define('LATEST_PH_RELEASE_DATE', '26th of August 2011');
+	define('LATEST_PH_VERSION', '2.21');
+	define('LATEST_PH_RELEASE_DATE', '2nd of September 2011');
 
-	define('LATEST_PH_SETUP_SIZE', '1.6 MB');
-	define('LATEST_PH_SETUP_SHA1', 'df408023f833a5e2f42cf1eaf3d68f2eae412f15');
-	define('LATEST_PH_SETUP_MD5', '1f5638bdf3332436a8d7b51a00ee2779');
+	define('LATEST_PH_SETUP_SIZE', '1.7 MB');
+	define('LATEST_PH_SETUP_SHA1', '796aa7ef28b3f4df87c6937ef53b5a76ccf91e59');
+	define('LATEST_PH_SETUP_MD5', '21eac181043ac4ccbb8d9c5d7455c783');
 
-	define('LATEST_PH_BIN_SIZE', '1.9 MB');
-	define('LATEST_PH_BIN_SHA1', '4b864831ae43ab6d52464375a01d4e30efac6932');
-	define('LATEST_PH_BIN_MD5', 'ad79e5c25db10f033a94b3c5706cba36');
+	define('LATEST_PH_BIN_SIZE', '2.0 MB');
+	define('LATEST_PH_BIN_SHA1', '9091926def34371fc0a610eb2870a499dca6e045');
+	define('LATEST_PH_BIN_MD5', '3b933398e0e9858f970cc49b49bdbde6');
 
-	define('LATEST_PH_SOURCE_SIZE', '2.2 MB');
-	define('LATEST_PH_SOURCE_SHA1', 'c158c741aa7fbe22cdba1e654e6851c6fe0bc2da');
-	define('LATEST_PH_SOURCE_MD5', 'de434182744003c9c4e57a4031306aff');
+	define('LATEST_PH_SOURCE_SIZE', '2.4 MB');
+	define('LATEST_PH_SOURCE_SHA1', '7c310c7588120203e5c8544872b2e1fcae9813c9');
+	define('LATEST_PH_SOURCE_MD5', '34a46a75f813dd5eff3d867af698d78e');
 
-	define('LATEST_PH_SDK_SIZE', '2.2 MB');
-	define('LATEST_PH_SDK_SHA1', 'd6b3efb9293c2be0d9aa91d3eae5d55baa3235f9');
-	define('LATEST_PH_SDK_MD5', 'edb2c10af6c2e5d89f13ee600069fa07');
+	define('LATEST_PH_SDK_SIZE', '2.4 MB');
+	define('LATEST_PH_SDK_SHA1', 'cd6e03dc768f6f9a0b55e2c90b274f7cbdb34536');
+	define('LATEST_PH_SDK_MD5', '7422335aec89a5beff95d96a910b1ccc');
 ?>
