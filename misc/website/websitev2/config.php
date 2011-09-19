a91e6fe70e7496ccce717a9897eaa6fc45cab0ba
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-09-19 10:37:36 +0000

    updated for 2.22 release
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@4697 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/config.php
+++ misc/website/websitev2/config.php
@@ -1,20 +1,20 @@
 <?php
-	define('LATEST_PH_VERSION', '2.21');
-	define('LATEST_PH_RELEASE_DATE', '2nd of September 2011');
+	define('LATEST_PH_VERSION', '2.22');
+	define('LATEST_PH_RELEASE_DATE', '19th of September 2011');
 
 	define('LATEST_PH_SETUP_SIZE', '1.7 MB');
-	define('LATEST_PH_SETUP_SHA1', '796aa7ef28b3f4df87c6937ef53b5a76ccf91e59');
-	define('LATEST_PH_SETUP_MD5', '21eac181043ac4ccbb8d9c5d7455c783');
+	define('LATEST_PH_SETUP_SHA1', '0023a7b9a282327b55715aa6caa4eb46996c1e5a');
+	define('LATEST_PH_SETUP_MD5', '8b6078f6ca09287303a4199b1b1afeb7');
 
 	define('LATEST_PH_BIN_SIZE', '2.0 MB');
-	define('LATEST_PH_BIN_SHA1', '9091926def34371fc0a610eb2870a499dca6e045');
-	define('LATEST_PH_BIN_MD5', '3b933398e0e9858f970cc49b49bdbde6');
+	define('LATEST_PH_BIN_SHA1', 'e689b5e72ba0e8007f688b1459ab454afd447cbd');
+	define('LATEST_PH_BIN_MD5', 'f27b6eceb3510ce41fd8383ef5dd8aaf');
 
 	define('LATEST_PH_SOURCE_SIZE', '2.4 MB');
-	define('LATEST_PH_SOURCE_SHA1', '7c310c7588120203e5c8544872b2e1fcae9813c9');
-	define('LATEST_PH_SOURCE_MD5', '34a46a75f813dd5eff3d867af698d78e');
+	define('LATEST_PH_SOURCE_SHA1', 'd224718c4142db673cc1210a7841f4cae3142b10');
+	define('LATEST_PH_SOURCE_MD5', 'ef581a40981f0202366d8b0898b36a2b');
 
 	define('LATEST_PH_SDK_SIZE', '2.4 MB');
-	define('LATEST_PH_SDK_SHA1', 'cd6e03dc768f6f9a0b55e2c90b274f7cbdb34536');
-	define('LATEST_PH_SDK_MD5', '7422335aec89a5beff95d96a910b1ccc');
+	define('LATEST_PH_SDK_SHA1', '23c9dc21aa4628bf911b768f4b242c10c54cd01c');
+	define('LATEST_PH_SDK_MD5', 'd1cf4c0609b6c4d2cee2217bd89b479e');
 ?>
