e9b2484501712a714aac7a7fd5af6cf5da6fb25e
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-06-03 20:32:13 +0000

    Website: redesigned about page
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5030 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/about.php
+++ misc/website/websitev2/about.php
@@ -8,14 +8,14 @@ include("header.php");
     <div class="yui-d0">
         <div class="watermark-apps-portlet">
             <div class="flowed-block">
-                <img alt="" width="64" height="64" src="/images/logo_64x64.png">
+                <img alt="ProjectLogo" width="64" height="64" src="/images/logo_64x64.png">
             </div>
-
+            
             <div class="flowed-block wide">
                 <h2>Process Hacker</h2>
-
-                <ul class="facetmenu"> 
-                    <li><a href="/">Overview</a></li> 
+                
+                <ul class="facetmenu">
+                    <li><a href="/">Overview</a></li>
                     <li><a href="/features.php">Features</a></li>
                     <li><a href="/screenshots.php">Screenshots</a></li>
                     <li><a href="/downloads.php">Downloads</a></li>
@@ -25,62 +25,105 @@ include("header.php");
                 </ul>
             </div>
         </div>
-    </div>	
-    
-    <div class="yui-t4">
-        <div class="top-portlet"> 
-            <p><strong>Registered:</strong> 16-10-2008</p>
-            <p><strong>Licence:</strong> GNU General Public License version 3.0 <a href="http://processhacker.svn.sourceforge.net/viewvc/processhacker/2.x/trunk/LICENSE.txt">GPLv3</a></p>
-            <p><strong>Language:</strong> English</p>
-            <p><strong>Intended Audience:</strong> Advanced End Users, Developers</p>
-            <p><strong>Programming Language:</strong> C, C#</p>
-            
-            <a href="http://sourceforge.net/project/memberlist.php?group_id=242527">
-                <p><strong>Project Members:</strong></p>
-            </a>
- 
-            <div class="portlet">
+        
+        <div class="yui-t4">
+            <div class="yui-b side">
+                <div class="portlet" >
+                    <h2 class="center">Quick Links</h2>
+                    <ul class="involvement">
+                        <li><a href="http://sourceforge.net/project/memberlist.php?group_id=242527">Project Members</a></li>
+                    </ul>
+                </div>
+                
+                <div class="portlet" >
+                    <h2 class="center">Get Involved</h2>
+                    <ul class="involvement">
+                        <li>
+                            <a href="/forums/viewforum.php?f=24">Report a bug</a>
+                        </li>
+                        <li>
+                            <a href="/forums/viewforum.php?f=5">Ask a question</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
- 
+            
+            <div class="top-portlet">
+                <div class="summary" style="padding-bottom:20px;">
+                    <p><strong>Registered:</strong> 16-10-2008</p>
+                    <p><strong>Licence:</strong> GNU General Public License version 3.0 <a href="http://processhacker.svn.sourceforge.net/viewvc/processhacker/2.x/trunk/LICENSE.txt">GPLv3</a></p>
+                    <p><strong>Language:</strong> English</p>
+                    <p><strong>Intended Audience:</strong> Advanced End Users, Developers</p>
+                    <p><strong>Programming Language:</strong> C, C#</p>
+                </div>
+            </div>
+            
             <div class="yui-g">
                 <div class="yui-u first">
-                    <img style="float: left; margin: 0 10px 5px 0;" src="http://www.gravatar.com/avatar/4dab4ece81a9b8832323ef2f73fcf12e" alt="User avatar">
-                    <li style="color:#AA0000" href="http://sourceforge.net/users/wj32/"><strong>wj32</strong></li>
-                    <a href="http://sourceforge.net/users/wj32/">
-                        <strong>User Profile</strong>
-                    </a>
+                    <div class="portlet" style="position:relative;">
+                        <img style="float:left; margin:15px 10px 0 0;" src="http://www.gravatar.com/avatar/4dab4ece81a9b8832323ef2f73fcf12e" alt="User avatar">
+                        <p>
+                            <a style="color:#AA0000" href="http://sourceforge.net/users/wj32/">
+                                <strong>wj32</strong>
+                            </a>
+                        </p>
+                        <ul>
+                            <li>Project Founder</li>
+                            <li>Joined: 16-10-2008</li>
+                            <li>Status: Active</li>
+                        </ul>
+                        
+                        <a href='https://www.ohloh.net/accounts/47453' target='_top'>
+                            <img style="position:absolute; top:10px; right:0;" alt='Ohloh profile for wj32' border='0' height='24' width='32' src='https://www.ohloh.net/accounts/47453/widgets/account_rank.gif' />
+                        </a>
+                        <a href='https://www.ohloh.net/p/processhacker/contributors/1415904688650589' target='_top'>
+                            <img style="position:absolute; bottom:0; right:0;" align="right" alt="wj32 commit history" height="32" width="179" src="https://www.ohloh.net/p/processhacker/contributors/144925/commits_spark.png" />
+                        </a>
+                    </div>
+                    
+                    <div class="portlet" style="position:relative;">
+                        <img style="float:left; margin: 15px 10px 0 0;" src="http://www.gravatar.com/avatar/989904e05fb257009009d2c6e2c8808f" alt="User avatar">
+                        <p>    
+                            <a style="color:#AA0000" href="http://sourceforge.net/users/dmex/">
+                                <strong>dmex</strong>
+                            </a>
+                        </p>
+                        <ul>
+                            <li>Director of Operations</li>
+                            <li>Joined: 28-08-2009</li>
+                            <li>Status: Active</li>
+                        </ul>
+                        
+                        <a href='https://www.ohloh.net/accounts/56334' target='_top'>
+                            <img style="position:absolute; top: 10px; right: 0;" alt='Ohloh profile for dmex' border='0' height='24' width='32' src='https://www.ohloh.net/accounts/56334/widgets/account_rank.gif' />
+                        </a>
+                        <a href='https://www.ohloh.net/p/processhacker/contributors/1415904688659470' target='_top'>
+                            <img style="position:absolute; bottom:0; right:0;" align="right" alt="dmex commit history" height="32" width="179" src="https://www.ohloh.net/p/processhacker/contributors/368201/commits_spark.png" />
+                        </a>
+                    </div>	
+                    
+                    <div class="portlet" style="position:relative;">
+                        <img style="float:left; margin: 15px 10px 0 0;" src="./images/mpc-hc_gabest_logo.png" width="80" height="80" alt="User avatar">
+                        <p>
+                            <a style="color:#0000FF" href="http://sourceforge.net/users/xhmikosr/">
+                                <strong>XhmikosR</strong>
+                            </a>
+                        </p>
+                        <ul>
+                            <li>Installer Setup Maintainer</li>
+                            <li>Joined: 20-02-2009</li>
+                            <li>Status: Active</li>
+                        </ul>
+                        
+                        <a href='https://www.ohloh.net/accounts/60504' target='_top'>
+                            <img style="position:absolute; top: 10px; right: 0;" alt='Ohloh profile for XhmikosR' border='0' height='24' width='32' src='https://www.ohloh.net/accounts/60504/widgets/account_rank.gif' />
+                        </a>
+                        <a href='https://www.ohloh.net/p/processhacker/contributors/1415904688663640' target='_top'>
+                            <img style="position:absolute; bottom:0; right:0;" align="right" alt="dmex commit history" height="32" width="179" src="https://www.ohloh.net/p/processhacker/contributors/145086/commits_spark.png" />
+                        </a>
+                    </div>
                 </div>
             </div>
-
-            <div class="portlet">
-            </div>
-
-            <div class="yui-g">	
-                <div class="yui-u first">
-                    <img style="float: left; margin: 0 10px 5px 0;" src="http://www.gravatar.com/avatar/989904e05fb257009009d2c6e2c8808f" alt="User avatar">
-                    <li style="color:#AA0000" href="http://sourceforge.net/users/dmex/"><strong>dmex</strong></li>
-                    <a href="http://sourceforge.net/users/dmex/">
-                        <strong>User Profile</strong>
-                    </a>
-                </div>
-            </div>
-
-            <div class="portlet">
-            </div>
-
-            <div class="yui-g">
-                <div class="yui-u first">
-                    <img style="float: left; margin: 0 10px 5px 0;" src="./images/mpc-hc_gabest_logo.png" width="64" height="64" alt="User avatar">
-                    <li style="color:#0000FF" href="http://sourceforge.net/users/xhmikosr/"><strong>XhmikosR</strong></li>
-                    <a href="http://sourceforge.net/users/xhmikosr/">
-                        <strong>User Profile</strong>
-                    </a>
-                </div>
-            </div>
-            
-            <div class="portlet">
-            </div>
-            
         </div>
     </div>
 </div>
