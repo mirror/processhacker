a2ba49f017b5c773a149fef74a54d52ab7bfcb91
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-12-07 17:23:47 +0000

    Clean up header.php and footer.php: * X-UA-Compatible is set in the .htaccess now so remove it from here. * type="text/css" isn't needed for stylesheets * add a title in the sf logo * reindent inline js
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5133 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/footer.php
+++ misc/website/websitev2/footer.php
@@ -1,5 +1,5 @@
 <div class="center">
-    <img src="http://sflogo.sourceforge.net/sflogo.php?group_id=242527&amp;type=13" width="120" height="30" alt="Process Hacker is Hosted by SourceForge.net">
+    <img src="http://sflogo.sourceforge.net/sflogo.php?group_id=242527&amp;type=13" width="120" height="30" alt="SourceForge logo" title="Process Hacker is Hosted by SourceForge.net">
     <br>
     Copyright &copy; 2008-2012 wj32
 </div>
@@ -10,12 +10,12 @@
     _gaq.push(['_setAccount', 'UA-22023876-1']);
     _gaq.push(['_trackPageview']);
     (function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+		var ga = document.createElement('script');
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
     })();
 </script>
 <!-- End Google Analytics -->
--- misc/website/websitev2/header.php
+++ misc/website/websitev2/header.php
@@ -1,24 +1,15 @@
-<?php header('Content-Type: text/html; charset=UTF-8');
-
+<?php
 include("config.php");
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta name="description" content="A free and open source process viewer with powerful process termination and memory searching/editing capabilities." />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8" />
-    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9">
-
-    <link rel="icon" type="image/ico" href="/images/favicon.ico">
-    <link rel="stylesheet" type="text/css" media="screen, print" href="/css/stylesheet.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="/css/lytebox.css" />
-
-    <link rel="alternate" type="application/atom+xml" href="http://processhacker.sourceforge.net/forums/feed.php?f=1" title="Process Hacker - News Atom Feed" />
-
     <title><?php echo $pagetitle ?> - Process Hacker</title>
+    <meta name="description" content="A free and open source process viewer with powerful process termination and memory searching/editing capabilities." />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/ico" href="/images/favicon.ico">
+    <link rel="stylesheet" media="screen, print" href="/css/stylesheet.css" />
+    <link rel="stylesheet" media="screen" href="/css/lytebox.css" />
+    <link rel="alternate" type="application/atom+xml" href="http://processhacker.sourceforge.net/forums/feed.php?f=1" title="Process Hacker - News Atom Feed" />
 </head>
-
 <body>
