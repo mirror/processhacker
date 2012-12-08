6ccc7f6bbde99d83c4405722e431f3a7caca628f
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-12-08 13:02:58 +0000

    move favicon.ico in root dir and remove the redirection
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5153 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/header.php
+++ misc/website/websitev2/header.php
@@ -5,7 +5,7 @@
     <title><?php echo $pagetitle ?> - Process Hacker</title>
     <meta name="description" content="A free and open source process viewer with powerful process termination and memory searching/editing capabilities." />
     <meta charset="utf-8" />
-    <link rel="icon" type="image/ico" href="/images/favicon.ico">
+    <link rel="icon" type="image/ico" href="/favicon.ico">
     <link rel="stylesheet" href="/css/pack.css" />
     <link rel="alternate" type="application/atom+xml" href="http://processhacker.sourceforge.net/forums/feed.php?f=1" title="Process Hacker - News Atom Feed" />
     <!--[if lt IE 9]>
