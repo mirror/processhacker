9b1d12c22c31eaba7d544d3cb9e999913b531180
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-12-09 04:47:45 +0000

    Added RSS feed
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5161 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/header.php
+++ misc/website/websitev2/header.php
@@ -7,7 +7,8 @@
     <meta charset="utf-8" />
     <link rel="icon" type="image/ico" href="/favicon.ico">
     <link rel="stylesheet" href="/css/pack.css" />
-    <link rel="alternate" type="application/atom+xml" href="http://processhacker.sourceforge.net/forums/feed.php?f=1" title="Process Hacker - News Atom Feed" />
+    <link rel="alternate" type="application/atom+xml" href="http://processhacker.sourceforge.net/forums/feed.php?f=1" title="Process Hacker - News" />
+    <link rel="alternate" type="application/atom+xml" href="https://sourceforge.net/p/processhacker/code/feed" title="Process Hacker - SVN" />
     <!--[if lt IE 9]>
     <script src="/js/html5shiv.js"></script>
     <![endif]-->
