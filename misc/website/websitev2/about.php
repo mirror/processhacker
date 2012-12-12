e456dbb439dfc5a9e047cbcc9678470973e0e268
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-12-12 07:02:07 +0000

    revert r5188, it broke the layout.
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5192 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/about.php
+++ misc/website/websitev2/about.php
@@ -1,63 +1,61 @@
-<?php $pagetitle = "About"; include("header.php"); ?>
+<?php $pagetitle = "About"; include "header.php"; ?>
 
-<div class="page">
+<div id="page">
     <div class="yui-d0">
-        <div class="watermark-apps-portlet">
-            <div class="flowed-block">
-                <img src="/images/logo_64x64.png" alt="Project Logo" width="64" height="64">
-            </div>
+        <nav>
+            <img class="flowed-block" src="images/logo_64x64.png" alt="Project Logo" width="64" height="64">
 
-            <div class="flowed-block wide">
+            <div class="flowed-block">
                 <h2>Process Hacker</h2>
                 <ul class="facetmenu">
                     <li><a href="/">Overview</a></li>
-                    <li><a href="/features.php">Features</a></li>
-                    <li><a href="/screenshots.php">Screenshots</a></li>
-                    <li><a href="/downloads.php">Downloads</a></li>
-                    <li><a href="/faq.php">FAQ</a></li>
-                    <li class="overview active"><a href="/about.php">About</a></li>
-                    <li><a href="/forums/">Forum</a></li>
+                    <li><a href="features.php">Features</a></li>
+                    <li><a href="screenshots.php">Screenshots</a></li>
+                    <li><a href="downloads.php">Downloads</a></li>
+                    <li><a href="faq.php">FAQ</a></li>
+                    <li class="active"><a href="about.php">About</a></li>
+                    <li><a href="forums/">Forum</a></li>
                 </ul>
             </div>
-        </div>
+        </nav>
 
         <div class="yui-t4">
             <div class="yui-b side">
-                <div class="portlet" >
+                <div class="portlet">
                     <h2 class="center">Quick Links</h2>
                     <ul class="involvement">
-                        <li><a href="http://sourceforge.net/project/memberlist.php?group_id=242527">Project Members</a></li>
-                        <li><a href="https://www.ohloh.net/p/processhacker/contributors">Project Contributors</a></li>
+                        <li><a href="http://www.ohloh.net/p/processhacker/contributors">Project Contributors</a></li>
                     </ul>
                 </div>
 
-                <div class="portlet" >
+                <div class="portlet">
                     <h2 class="center">Get Involved</h2>
                     <ul class="involvement">
-                        <li><a href="/forums/viewforum.php?f=24">Report a bug</a></li>
-                        <li><a href="/forums/viewforum.php?f=5">Ask a question</a></li>
+                        <li><a href="forums/viewforum.php?f=24">Report a bug</a></li>
+                        <li><a href="forums/viewforum.php?f=5">Ask a question</a></li>
                     </ul>
                 </div>
             </div>
 
-            <div class="top-portlet">
-                <div class="summary" style="padding-bottom:20px">
-                    <p>Process Hacker was started in 2008 as an open source alternative to programs such as Task Manager and Process Explorer.</p>
-                    <p><strong>Registered:</strong> 16-10-2008</p>
-                    <p><strong>Licence:</strong> GNU General Public License version 3.0 <a href="http://processhacker.svn.sourceforge.net/viewvc/processhacker/2.x/trunk/LICENSE.txt">GPLv3</a></p>
-                    <p><strong>Language:</strong> English</p>
-                    <p><strong>Intended Audience:</strong> Advanced End Users, Developers</p>
-                    <p><strong>Programming Language:</strong> C, C#</p>
-                </div>
+            <div id="about">
+                <p>Process Hacker was started in 2008 as an open source alternative to programs such as Task Manager and Process Explorer.</p>
+                <ul>
+                    <li class="about-li"><strong>Registered:</strong> 16-10-2008</li>
+                    <li class="about-li"><strong>Licence:</strong> GNU General Public License version 3.0
+                        <a href="http://processhacker.svn.sourceforge.net/viewvc/processhacker/2.x/trunk/LICENSE.txt">GPLv3</a></li>
+                    <li class="about-li"><strong>Language:</strong> English</li>
+                    <li class="about-li"><strong>Intended Audience:</strong> Advanced End Users, Developers</li>
+                    <li class="about-li"><strong>Programming Language:</strong> C, C#</li>
+                </ul>
             </div>
 
             <div class="yui-g">
                 <div class="yui-u first">
-                    <div class="portlet" style="position:relative">
-                        <a href="http://wj32.org">
-                            <img class="user-avatar" src="http://www.gravatar.com/avatar/4dab4ece81a9b8832323ef2f73fcf12e" alt="User avatar">
-                            <p><strong style="color:#AA0000">wj32</strong></p>
-                        </a>
+                    <div class="portlet dev">
+                        <p>
+                            <img class="user-avatar" src="http://www.gravatar.com/avatar/4dab4ece81a9b8832323ef2f73fcf12e" alt="User avatar" width="80" height="80">
+                            <a href="http://sourceforge.net/users/wj32/" style="color:#AA0000"><strong>wj32</strong></a>
+                        </p>
                         <ul>
                             <li>Project Founder</li>
                             <li>Joined: 16-10-2008</li>
@@ -65,60 +63,52 @@
                             <li><a href="http://processhacker.sourceforge.net/forums/memberlist.php?mode=email&amp;u=2">Send Message</a></li>
                         </ul>
 
-                        <a href="http://meta.ohloh.net/kudos/" target="_top">
-                            <img class="ohloh-kudos" alt="Ohloh kudos" src="https://www.ohloh.net/accounts/47453/widgets/account_rank.gif" />
-                        </a>
-                        <a href="https://www.ohloh.net/p/processhacker/contributors/1415904688650589" target="_top">
-                            <img class="ohloh-commits" alt="wj32 commit history" height="32" width="179" src="https://www.ohloh.net/p/processhacker/contributors/144925/commits_spark.png" />
+                        <img class="ohloh-kudos" src="http://www.ohloh.net/accounts/47453/widgets/account_rank.gif" alt="Ohloh kudos" width="32" height="24"/>
+
+                        <a href="http://www.ohloh.net/p/processhacker/contributors/1415904688650589" target="_blank">
+                            <img class="ohloh-commits" src="http://www.ohloh.net/p/processhacker/contributors/144925/commits_spark.png" alt="wj32 commit history" width="182" height="32"/>
                         </a>
                     </div>
 
-                    <div class="portlet" style="position:relative">
-                        <a href="http://sourceforge.net/users/dmex/">
-                            <img class="user-avatar" src="http://www.gravatar.com/avatar/989904e05fb257009009d2c6e2c8808f" alt="User avatar">
-                            <p style="color:#AA0000">
-                                <strong>dmex</strong> - 0x0D06F00D
-                            </p>
-                        </a>
+                    <div class="portlet dev">
+                        <p>
+                            <img class="user-avatar" src="http://www.gravatar.com/avatar/989904e05fb257009009d2c6e2c8808f" alt="User avatar" width="80" height="80">
+                            <a href="http://sourceforge.net/users/dmex/" style="color:#AA0000"><strong>dmex</strong> - 0x0D06F00D</a>
+                        </p>
                         <ul>
-                            <li>Project Operations, Maintainer</li>
+                            <li>Project Maintainer</li>
                             <li>Joined: 28-08-2009</li>
                             <li>Status: Active</li>
                             <li><a href="http://processhacker.sourceforge.net/forums/memberlist.php?mode=email&amp;u=54">Send Message</a></li>
                         </ul>
 
-                        <a href="http://meta.ohloh.net/kudos/" target="_top">
-                            <img class="ohloh-kudos" alt="Ohloh kudos" src="https://www.ohloh.net/accounts/56334/widgets/account_rank.gif" />
-                        </a>
-                        <a href="https://www.ohloh.net/p/processhacker/contributors/1415904688659470" target="_top">
-                            <img class="ohloh-commits" alt="dmex commit history" height="32" width="179" src="https://www.ohloh.net/p/processhacker/contributors/368201/commits_spark.png" />
+                        <img class="ohloh-kudos" src="http://www.ohloh.net/accounts/56334/widgets/account_rank.gif" alt="Ohloh kudos" width="32" height="24"/>
+
+                        <a href="http://www.ohloh.net/p/processhacker/contributors/1415904688659470" target="_blank">
+                            <img class="ohloh-commits" src="http://www.ohloh.net/p/processhacker/contributors/368201/commits_spark.png" alt="dmex commit history" width="182" height="32"/>
                         </a>
                     </div>
 
-                      <div class="portlet" style="position:relative">
-                        <a href="http://sourceforge.net/users/xhmikosr/">
-                            <img class="user-avatar" src="http://www.gravatar.com/avatar/e37759b1ea0125d4e97b1e00b5eed26f.png" width="80" height="80" alt="User avatar">
-                            <p style="color:#0000FF">
-                                <strong>XhmikosR</strong>
-                            </p>
-                        </a>
+                      <div class="portlet dev">
+                        <p>
+                            <img class="user-avatar" src="http://www.gravatar.com/avatar/e37759b1ea0125d4e97b1e00b5eed26f.png" alt="User avatar" width="80" height="80">
+                            <a href="http://sourceforge.net/users/xhmikosr/" style="color:#0000FF"><strong>XhmikosR</strong></a>
+                        </p>
                         <ul>
-                            <li>Installer Maintainer</li>
+                            <li>Installer Maintainer, Website improvements</li>
                             <li>Joined: 20-02-2009</li>
                             <li>Status: Active</li>
                         </ul>
 
-                        <a href="http://meta.ohloh.net/kudos/" target="_top">
-                            <img class="ohloh-kudos" alt="Ohloh kudos" height="24" width="32" src="https://www.ohloh.net/accounts/60504/widgets/account_rank.gif" />
-                        </a>
-                        <a href="https://www.ohloh.net/p/processhacker/contributors/1415904688663640" target="_top">
-                            <img class="ohloh-commits" alt="XhmikosR commit history" height="32" width="179" src="https://www.ohloh.net/p/processhacker/contributors/145086/commits_spark.png" />
+                        <img class="ohloh-kudos" src="http://www.ohloh.net/accounts/60504/widgets/account_rank.gif" alt="Ohloh kudos" width="32" height="24"/>
+
+                        <a href="http://www.ohloh.net/p/processhacker/contributors/1415904688663640" target="_blank">
+                            <img class="ohloh-commits" src="http://www.ohloh.net/p/processhacker/contributors/145086/commits_spark.png" alt="XhmikosR commit history" width="182" height="32"/>
                         </a>
                     </div>
 
-
                     <div class="portlet">
-                        <script type="text/javascript" src="http://www.ohloh.net/p/329666/widgets/project_basic_stats.js"></script>
+                        <script src="http://www.ohloh.net/p/329666/widgets/project_basic_stats.js"></script>
                     </div>
                 </div>
             </div>
@@ -126,4 +116,4 @@
     </div>
 </div>
 
-<?php include("footer.php"); ?>
+<?php include "footer.php"; ?>
