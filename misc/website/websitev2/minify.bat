f172b83b689358a321466e3d3039972574aee4ff
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2013-04-02 13:36:18 +0000

    update normalize.css and html5shiv.js
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5289 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/minify.bat
+++ misc/website/websitev2/minify.bat
@@ -5,6 +5,6 @@ rem npm install -g clean-css
 pushd %~dp0
 
 echo minifying and combining css files...
-type css\stylesheet.css css\normalize.css | cleancss --s0 -o css\pack.css
+type css\stylesheet.css css\normalize.css | cleancss --s0 --remove-empty -o css\pack.css
 
 popd
