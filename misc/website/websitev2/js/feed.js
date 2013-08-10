f8584568b83182c292ff30826b700505fadb1e53
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2013-08-10 08:49:46 +0000

    Minor tweaks to the feed code and style.
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5415 21ef857c-d57f-4fe0-8362-d861dc6d29cd

--- misc/website/websitev2/js/feed.js
+++ misc/website/websitev2/js/feed.js
@@ -1,7 +1,5 @@
-var feedcontainer=document.getElementById("feeddiv");
-var rssoutput = "";
-
-feedcontainer.innerHTML = "<div id=\"feeddiv2\">Loading commit history...</div>";
+var feedcontainer = document.getElementById("feeddiv");
+feedcontainer.innerHTML = "<div>Loading commit history...</div>";
 
 function rssfeedsetup() {
     var feedpointer = new google.feeds.Feed("http://sourceforge.net/p/processhacker/code/feed");
@@ -11,13 +9,14 @@ function rssfeedsetup() {
 
 function displayfeed(result) {
     if (!result.error) {
+        var rssoutput = "";
         var thefeeds = result.feed.entries;
         for (var i = 0; i < thefeeds.length; i++) {
             rssoutput += "<div>";
             rssoutput += "<a href=\" " + thefeeds[i].link + " \">" +
                             thefeeds[i].title.replace("/p/processhacker/code/", "http://sourceforge.net/p/processhacker/code/") +
                          "</a>";
-            rssoutput += "<span class=\"forumdate\"> by <span style=\"color:#A00\">" + thefeeds[i].author + "</span></span>";
+            rssoutput += "<span class=\"forumdate\"> by <span class=\"author\">" + thefeeds[i].author + "</span></span>";
             rssoutput += "<div class=\"forumdate\">" + moment(thefeeds[i].publishedDate).fromNow() + " - " + new Date(thefeeds[i].publishedDate).toLocaleString() + "</div>";
             rssoutput += "</div>";
         }
