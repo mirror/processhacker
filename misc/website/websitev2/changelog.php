83b01343009546ebe127a24d5c22a465b0aac572
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2013-10-28 10:48:48 +0000

    Website: Re-added changelog.php with new Sourceforge URLs
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5503 21ef857c-d57f-4fe0-8362-d861dc6d29cd

---
+++ misc/website/websitev2/changelog.php
@@ -0,0 +1,31 @@
+<?php $pagetitle = "Changelog"; include "header.php"; ?>
+
+<div class="page">
+    <div class="yui-d0">
+        <div class="yui-t4">
+            <nav>
+                <div class="logo">
+                    <a href="/"><img class="flowed-block" src="img/logo_64x64.png" alt="Project Logo" width="64" height="64"></a>
+                </div>
+
+                <div class="flowed-block">
+                    <h2>Process Hacker</h2>
+                    <ul class="facetmenu">
+                        <li><a href="/">Overview</a></li>
+                        <li><a href="downloads.php">Downloads</a></li>
+                        <li><a href="faq.php">FAQ</a></li>
+                        <li><a href="forums/">Forum</a></li>
+                        <li><a href="about.php">About</a></li>
+                    </ul>
+                </div>
+            </nav>
+            
+            <div class="summary">
+                <p>This is the changelog from Process Hacker's SVN repository. As such it may contain information about unreleased versions of Process Hacker.</p>
+                <iframe class="changelog" src="http://svn.code.sf.net/p/processhacker/code/2.x/trunk/CHANGELOG.txt"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php"; ?>
