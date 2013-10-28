6a9ac8bb550daddd453e236074e6b6ee0246793d
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2013-10-28 23:21:18 +0000

    Websitev3: split about.php content
    git-svn-id: svn://svn.code.sf.net/p/processhacker/code@5507 21ef857c-d57f-4fe0-8362-d861dc6d29cd

---
+++ misc/website/websitev3/members.php
@@ -0,0 +1,113 @@
+<?php $pagetitle = "Members"; include "include/header.php"; ?>
+
+<div class="row">
+    <h1 class="page-header"><small>Project Members</small></h1>
+</div>
+
+<div class="row row-offcanvas row-offcanvas-right">
+    <div class="col-xs-12 col-sm-9">
+        <div class="row">
+            <a class="thumbnail pull-left" href="#">
+                <img class="media-object" src="http://www.gravatar.com/avatar/4dab4ece81a9b8832323ef2f73fcf12e">
+             </a>
+            
+            <div class="col-lg-6 col-md-6">
+                <div class="btn-group pull-right">
+                    <div class="btn btn-success dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></div>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://processhacker.sourceforge.net/forums/memberlist.php?mode=email&amp;u=2">Send private message</a></li>
+                        <li><a href="http://processhacker.sourceforge.net/forums/memberlist.php?mode=email&amp;u=2">E-mail wj32</a></li>
+                        <li class="divider"></li>
+                        <li><a target="_blank" href="http://wj32.org">http://wj32.org</a></li>
+                    </ul>
+                </div>
+            
+                <div class="media-body">
+                    <h4 class="media-heading">wj32</h4>
+                    <small><cite title="Source Title">Project Founder</cite></small>
+                    <p><span class="label label-info">Founded: 16-10-2008</span></p>
+                    <a class="thumbnail pull-left" href="http://www.ohloh.net/p/processhacker/contributors/1415904688650589" target="_blank">
+                        <img class="media-object" src="http://www.ohloh.net/accounts/47453/widgets/account_rank.gif" alt="Ohloh kudos" />
+                        <img class="media-object" src="http://www.ohloh.net/p/processhacker/contributors/144925/commits_spark.png" alt="wj32 commit history" width="182" height="32"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <br/>
+        
+        <div class="row">
+            <hr>
+            <a class="thumbnail pull-left" href="http://sourceforge.net/users/dmex/">
+                <img class="media-object" src="http://www.gravatar.com/avatar/989904e05fb257009009d2c6e2c8808f">
+            </a>
+            
+            <div class="col-lg-6 col-md-6">
+                <div class="btn-group pull-right">
+                    <div class="btn btn-success dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></div>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://processhacker.sourceforge.net/forums/memberlist.php?mode=email&amp;u=54">Send Message</a></li>
+                        <li><a href="">Send Email</a></li>
+                        <li class="divider"></li>
+                        <li><a target="_blank" href="http://sourceforge.net/users/dmex/">View Website</a></li>
+                    </ul>
+                </div>
+            
+                <div class="media-body">
+                    <h4 class="media-heading">dmex</h4>
+                    <small><cite title="Source Title">Project Maintainer, Website Maintainer</cite></small>
+                    <p><span class="label label-info">Joined: 28-08-2009</span></p>
+                    <a class="thumbnail pull-left" href="http://www.ohloh.net/p/processhacker/contributors/1415904688659470" target="_blank">
+                        <img class="media-object" src="http://www.ohloh.net/accounts/56334/widgets/account_rank.gif" alt="Ohloh kudos" />
+                        <img class="media-object" src="http://www.ohloh.net/p/processhacker/contributors/368201/commits_spark.png" alt="dmex commit history" width="182" height="32"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <br/>
+        
+        <div class="row">
+            <hr>
+            <a class="thumbnail pull-left" href="http://sourceforge.net/users/xhmikosr/">
+                <img class="media-object" src="http://www.gravatar.com/avatar/e37759b1ea0125d4e97b1e00b5eed26f">
+            </a>
+            
+            <div class="col-lg-6 col-md-6">
+                <div class="btn-group pull-right">
+                    <div class="btn btn-success dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></div>
+                    <ul class="dropdown-menu">
+                        <li><a href="">Send Message</a></li>
+                        <li><a href="">Send Email</a></li>
+                        <li class="divider"></li>
+                        <li><a target="_blank" href="http://sourceforge.net/users/xhmikosr/">View Website</a></li>
+                    </ul>
+                </div>
+                
+                <div class="media-body">
+                    <h4 class="media-heading">XhmikosR</h4>
+                    <small><cite title="Source Title">Installer Maintainer, Website improvements</cite></small>
+                    <p><span class="label label-info">Joined: 20-02-2009</span></p>
+                    <a class="thumbnail pull-left" href="http://www.ohloh.net/p/processhacker/contributors/1415904688663640" target="_blank">
+                        <img class="media-object" src="http://www.ohloh.net/accounts/60504/widgets/account_rank.gif" alt="Ohloh kudos" />
+                        <img class="media-object" src="http://www.ohloh.net/p/processhacker/contributors/145086/commits_spark.png" alt="XhmikosR commit history" width="182" height="32"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+          <div class="well sidebar-nav">
+            <ul class="nav">
+                <li><a href="http://sourceforge.net/projects/processhacker/">SourceForge project page</a></li>
+                <li><a href="forums/viewforum.php?f=5">Ask a question</a></li>
+                <li><a href="forums/viewforum.php?f=24">Report a bug</a></li>
+                <li><a href="http://sourceforge.net/p/processhacker/code/">Browse source code</a></li>
+                <li><a href="doc/">Source code documentation</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+ 
+<?php include "include/footer.php"; ?>
